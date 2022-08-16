766c694019be36aae35b4cc851f0316ee5ea2739
Mateo Armoa <tutecarc98@hotmail.com>
2022-08-16 17:12:21 -0300

    subida al github

---
+++ JS/menu.js
@@ -0,0 +1,145 @@
+[
+    {
+        "type": "Entradas",
+        "description": "Aros de cebolla",
+        "price": 600,
+        "available": true,
+        "source":"imagenes/aros-de-cebolla.jpg"
+    },
+    {
+        "type": "Entradas",
+        "description": "Nachos con cheddar",
+        "price": 500,
+        "available":true,
+        "source":"imagenes/nachos-cheddar.webp"
+    },
+    {
+        "type": "Entradas",
+        "description": "Papas fritas",
+        "price": 600,
+        "available":true,
+        "source":"imagenes/papas-fritas.webp"
+
+    },
+    {
+        "type": "Entradas",
+        "description": "Empanada",
+        "price": 250,
+        "available":true,
+        "source":"imagenes/empanada.jpg"
+
+    },
+    {
+        "type": "Entradas",
+        "description": "Rabas",
+        "price": 800,
+        "available":true,
+        "source":"imagenes/rabas.jpg"
+    },
+    {
+        "type": "Principales",
+        "description": "Milanesa con papas fritas",
+        "price": 1300,
+        "available":true,
+        "source":"imagenes/milanesa-con-fritas.jpg"
+    },
+    {
+        "type": "Principales",
+        "description": "Lomo al champignon con pure de papas",
+        "price": 1550,
+        "available":true,
+        "source":"imagenes/lomo.webp"
+    },
+    {
+        "type": "Principales",
+        "description": "Pizza 4 quesos",
+        "price": 1650,
+        "available":true,
+        "source":"imagenes/pizza-4-quesos.jpg"
+    },
+    {
+        "type": "Principales",
+        "description": "Pechuga al limon con ensalada (2 gustos)",
+        "price": 1250,
+        "available":true,
+        "source":"imagenes/pechuga-al-limon.jpg"
+    },
+    {
+        "type": "Principales",
+        "description": "Spaghettis con salsa (A eleccion)",
+        "price": 1200,
+        "available":true,
+        "source":"imagenes/spaghettis.jpg"
+    },
+    {
+        "type": "Postres",
+        "description": "Frutillas con crema",
+        "price": 790,
+        "available":true,
+        "source":"imagenes/frutillas-con-crema.jpg"
+    },
+    {
+        "type": "Postres",
+        "description": "Budin de pan",
+        "price": 500,
+        "available":true,
+        "source":"imagenes/budin-de-pan-con-dulce.jpg"
+    },
+    {
+        "type": "Postres",
+        "description": "Flan casero",
+        "price": 520,
+        "available":true,
+        "source":"imagenes/flan-casero.webp"
+    },
+    {
+        "type": "Postres",
+        "description": "Ensalada de frutas",
+        "price": 590,
+        "available":true,
+        "source":"imagenes/ensalada-de-frutas.webp"
+    },
+    {
+        "type": "Postres",
+        "description": "Helado (2 bochas)",
+        "price": 560,
+        "available":true,
+        "source":"imagenes/helado-dos-bochas.jpg"
+    },
+    {
+        "type": "Bebidas",
+        "description": "Agua sin gas",
+        "price": 210,
+        "available":true,
+        "source":"imagenes/agua-mineral.jpg"
+    },
+    {
+        "type": "Bebidas",
+        "description": "Gaseosa (Linea Pepsi)",
+        "price": 400,
+        "available":true,
+        "source":"imagenes/pepsi.webp"
+    },
+    {
+        "type": "Bebidas",
+        "description": "Cerveza Quilmes",
+        "price": 500,
+        "available":true,
+        "source":"imagenes/cerveza-quilmes.jpeg"
+        
+    },
+    {
+        "type": "Bebidas",
+        "description": "Vino",
+        "price": 1100,
+        "available":true,
+        "source":"imagenes/vino.jpeg"
+    },
+    {
+        "type": "Bebidas",
+        "description": "Vaso de Fernet",
+        "price": 600,
+        "available":true,
+        "source":"imagenes/vaso-fernet.jpg"
+    }
+]
